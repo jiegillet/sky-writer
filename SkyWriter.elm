88bb8ed4be9622f8aa1861dce705d7347b79ca1b
Jérémie Gillet <jie.gillet@gmail.com>
2018-03-28 19:30:27 +0900

    Finished letters and added animations

--- SkyWriter.elm
+++ SkyWriter.elm
@@ -3,15 +3,20 @@ module SkyWriter exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (placeholder, align)
 import Html.Events exposing (onClick, onInput)
-import Svg exposing (svg, circle, line, mask, rect)
+import Svg exposing (Svg, svg, circle, line, mask, rect)
 import Svg.Attributes exposing (..)
+import Time.DateTime as T exposing (DateTime)
 import Http
 import Json.Decode as Decode
 import Result
-import Time.DateTime as T exposing (DateTime)
+import Maybe
+import Array exposing (Array)
 import Dict
-import Anim exposing (..)
-import Letters exposing (alphabet)
+import List.Extra exposing (minimumBy)
+import Animation
+import Color
+import Letters exposing (Letter, alphabet)
+import Debug
 
 
 main =
@@ -34,8 +39,9 @@ type alias Model =
     , stars : List Star
     , stars2D : List Position
     , day : DateTime
-    , circles : Anim.Circles
-    , segments : Anim.Segments
+    , circles : Circles
+    , segments : Segments
+    , screen : Sreen
     , error : String
     }
 
@@ -52,28 +58,51 @@ type alias Position =
     ( Float, Float, Float )
 
 
+type alias Circles =
+    Array Circle
+
+
+type alias Circle =
+    { style : Animation.State, x : Float, y : Float, mag : Float }
+
+
+type alias Segments =
+    List Segment
+
+
+type alias Segment =
+    { style : Animation.State, from : Int, to : Int }
+
+
+type Sreen
+    = Info
+    | StarMap
+
+
 
 -- INIT
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model ""
-        defaultLoc
-        "SHINJI"
-        []
-        []
-        j2000
-        Anim.emptyCircles
-        Anim.emptySegments
-        ""
+    ( { address = ""
+      , location = defaultLoc
+      , name = "GAKKUN"
+      , stars = []
+      , stars2D = []
+      , day = parseTime "2017-06-14"
+      , circles = Array.empty
+      , segments = []
+      , screen = Info
+      , error = ""
+      }
     , getStarData
     )
 
 
 defaultLoc : Location
 defaultLoc =
-    Location "North Pole" 90 0
+    Location "Naha, Okinawa" 26.213 127.679
 
 
 
@@ -82,10 +111,9 @@ defaultLoc =
 
 subscriptions : Model -> Sub Msg
 subscriptions { circles, segments } =
-    Sub.batch
-        [ Anim.subscriptionsCircles Animate circles
-        , Anim.subscriptionsSegments Animate segments
-        ]
+    Animation.subscription Animate <|
+        (Array.toList <| Array.map .style circles)
+            ++ List.map .style segments
 
 
 
@@ -99,7 +127,7 @@ type Msg
     | ReadDate String
     | NewStars (Result Http.Error (List Star))
     | NewLocation (Result Http.Error Location)
-    | Animate Anim.Msg
+    | Animate Animation.Msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -131,25 +159,33 @@ update msg model =
                 stars2D =
                     List.map (getPosition location model.day) model.stars
 
-                ( circles, segments ) =
+                ( circ, seg ) =
                     initAnim stars2D model.name
             in
                 ( { model
                     | location = location
                     , stars2D = stars2D
-                    , circles = circles
-                    , segments = segments
+                    , circles = animateCircles circ
+                    , segments = animateSegments circ seg
+                    , screen = StarMap
                   }
                 , Cmd.none
                 )
 
         Animate animMsg ->
-            ( { model
-                | circles = Anim.updateCircles animMsg model.circles
-                , segments = Anim.updateSegments animMsg model.segments
-              }
-            , Cmd.none
-            )
+            let
+                updateCircle animMsg circ =
+                    { circ | style = Animation.update animMsg circ.style }
+
+                updateSegment animMsg p =
+                    { p | style = Animation.update animMsg p.style }
+            in
+                ( { model
+                    | circles = Array.map (updateCircle animMsg) model.circles
+                    , segments = List.map (updateSegment animMsg) model.segments
+                  }
+                , Cmd.none
+                )
 
 
 
@@ -293,31 +329,43 @@ view model =
         [ Html.Attributes.style
             [ ( "backgroundColor", "#000" )
             , ( "height", "100%" )
+            , ( "color", "white" )
             ]
         , align "center"
         ]
-        [ div []
-            [ input [ placeholder "Nishihara, Okinawa", onInput ReadLoc ] []
-            , input [ placeholder "Shinji", onInput ReadName ] []
-            , input [ type_ "date", Html.Attributes.value "2017-06-14", onInput ReadDate ] []
-            , button [ onClick Render ] [ text "Ok" ]
-            ]
-        , svg
-            [ width "600"
-            , height "600"
-            , viewBox "0 0 600 600"
-            , stroke "white"
-            , fill "none"
-            , strokeWidth "0.5"
-            , Svg.Attributes.mask "url(#hole)"
-            ]
-            (circleMask
-                :: circle [ cx "300", cy "300", r "297", stroke "white", strokeWidth "5" ] []
-                :: drawParallels model.location
-                ++ drawMeridians model.location
-                ++ (List.map drawStar model.stars2D)
-            )
-        ]
+    <|
+        case model.screen of
+            Info ->
+                [ h1 [] [ text "Welcome" ]
+                , h3 [] [ text "What is your first name?" ]
+                , div [] [ input [ placeholder "Gakkun", onInput ReadName ] [] ]
+                , h3 [] [ text "Where were you born?" ]
+                , div [] [ input [ placeholder "Onna, Okinawa", onInput ReadLoc ] [] ]
+                , h3 [] [ text "What day were you born?" ]
+                , div [] [ input [ type_ "date", onInput ReadDate ] [] ]
+                , h3 [] []
+                , div [] [ button [ onClick Render ] [ text "Continue" ] ]
+                ]
+
+            StarMap ->
+                [ svg
+                    [ width "600"
+                    , height "600"
+                    , viewBox "0 0 600 600"
+                    , stroke "white"
+                    , fill "none"
+                    , strokeWidth "0.5"
+                    , Svg.Attributes.mask "url(#hole)"
+                    ]
+                    (circleMask
+                        :: circle [ cx "300", cy "300", r "297", stroke "white", strokeWidth "5" ] []
+                        :: drawParallels model.location
+                        ++ drawMeridians model.location
+                        ++ (List.map drawStar model.stars2D)
+                     -- ++ viewCircles model.circles
+                     -- ++ viewSegments model.segments
+                    )
+                ]
 
 
 
@@ -407,3 +455,175 @@ drawStar ( x, y, mag ) =
         , fill "white"
         ]
         []
+
+
+viewCircles : Circles -> List (Svg msg)
+viewCircles =
+    let
+        viewCircle { style, x, y, mag } =
+            circle
+                (Animation.render style
+                    ++ [ cx <| renorm x 1
+                       , cy <| renorm y 1
+                       , r <| toString (6 - mag)
+                       ]
+                )
+                []
+    in
+        Array.toList << Array.map viewCircle
+
+
+viewSegments : Segments -> List (Svg msg)
+viewSegments =
+    let
+        viewSegment : Segment -> Svg msg
+        viewSegment { style } =
+            Svg.path
+                (stroke "yellow" :: strokeWidth "2" :: Animation.render style)
+                []
+    in
+        List.map viewSegment
+
+
+
+-- ANIMATION
+
+
+initAnim : List ( Float, Float, Float ) -> String -> ( Circles, Segments )
+initAnim pos name =
+    let
+        theta =
+            atan2 4 (4 * (toFloat <| String.length name) - 1)
+
+        l =
+            0.9 * sin theta / 4
+
+        posIn =
+            List.filter (\( x, y, _ ) -> x ^ 2 + y ^ 2 <= 1) pos
+
+        closestPoint x0 ( xp, yp ) =
+            Maybe.withDefault ( 0, 0, 0 ) <|
+                minimumBy
+                    (\( x, y, _ ) ->
+                        (x - x0 - xp * l) ^ 2 + (y - yp * l + sin theta / 2) ^ 2
+                    )
+                    posIn
+
+        gather c ( x0, circ, seg ) =
+            let
+                { points, lines } =
+                    Maybe.withDefault (Letter [] []) (Dict.get c alphabet)
+
+                pts =
+                    List.map (closestPoint x0) points
+
+                lns =
+                    List.map
+                        (\( i, j ) ->
+                            ( i + List.length circ, j + List.length circ )
+                        )
+                        lines
+            in
+                ( x0 + 4 * l, circ ++ pts, seg ++ lns )
+
+        ( _, circ, seg ) =
+            List.foldl gather
+                ( (-1 / 2) * cos theta, [], [] )
+                (String.toList name)
+
+        mkCircles =
+            Array.fromList <|
+                List.map
+                    (\( x, y, m ) ->
+                        Circle
+                            (Animation.styleWith
+                                (Animation.spring { stiffness = 100, damping = 20 })
+                                [ Animation.strokeWidth 0
+                                , Animation.fill Color.white
+                                , Animation.stroke Color.white
+                                ]
+                            )
+                            x
+                            y
+                            m
+                    )
+                    circ
+
+        mkSegments circles =
+            List.map
+                (\( i, j ) ->
+                    Segment
+                        (Animation.styleWith
+                            (Animation.spring { stiffness = 10, damping = 5 })
+                            [ Animation.path
+                                [ Animation.moveTo 0 0, Animation.lineTo 0 0 ]
+                            ]
+                        )
+                        i
+                        j
+                )
+                seg
+    in
+        ( mkCircles, mkSegments mkCircles )
+
+
+getCoord : Int -> Circles -> ( Float, Float )
+getCoord n pts =
+    ( Maybe.withDefault 0 <| Maybe.map (\{ x } -> 600 * (1 / 2 + x)) <| Array.get n pts
+    , Maybe.withDefault 0 <| Maybe.map (\{ y } -> 600 * (1 / 2 + y)) <| Array.get n pts
+    )
+
+
+animateCircles : Circles -> Circles
+animateCircles =
+    let
+        animateCircle circ =
+            { circ
+                | style =
+                    Animation.queue
+                        [ Animation.wait <| 1000 + 100 * circ.x
+                        , Animation.to
+                            [ Animation.strokeWidth 15
+                            , Animation.fill Color.yellow
+                            , Animation.stroke Color.yellow
+                            ]
+                        , Animation.to [ Animation.strokeWidth 0 ]
+                        ]
+                        circ.style
+            }
+    in
+        Array.map animateCircle
+
+
+animateSegments : Circles -> Segments -> Segments
+animateSegments pts =
+    let
+        animateSegment points seg =
+            { seg
+                | style =
+                    let
+                        ( xf, yf ) =
+                            getCoord seg.from points
+
+                        ( xt, yt ) =
+                            getCoord seg.to points
+                    in
+                        Animation.interrupt
+                            [ Animation.wait <| 3000 + 10 * xf
+                            , Animation.set
+                                [ Animation.path
+                                    [ Animation.moveTo xf yf
+                                    , Animation.lineTo xf yf
+                                    ]
+                                ]
+                            , Animation.to
+                                [ Animation.path
+                                    [ Animation.moveTo xf yf
+                                    , Animation.lineTo xt yt
+                                    ]
+                                ]
+                            ]
+                            seg.style
+            }
+    in
+        List.map (animateSegment pts)
